355d16ee18216f3ec1af35773ab27bd2546c506a
vikas belida <vikasbelida@vikass-Air.lan>
2023-02-01 23:44:29 -0500

    resolved cors issues

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request,jsonify
 import sqlite3
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/register', methods=['POST'])
 def register_user():
@@ -25,13 +27,16 @@ def register_user():
     conn.commit()
     conn.close()
 
-    return jsonify(
-        firstname,
-        lastname,
-        email,
-        username,
-        passwd
-    )
+    response=jsonify({
+        'firstname':firstname,
+        'lastname':lastname,
+        'email':email,
+        'passwd':passwd,
+        'username':username
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
 @app.route("/get_users")
 def get_all_users():
     conn = sqlite3.connect('database.db')
@@ -56,7 +61,14 @@ def login():
     cursor.execute("SELECT * FROM users WHERE username=? AND passwd=?",(username,passwd))
     user = cursor.fetchone()
     if user:
-        return jsonify(user)
+        response=jsonify({
+            'firstname':user[2],
+            'lastname':user[3],
+            'email':user[4],
+            'username':user[6]
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     return "Invalid credentials"
 
 if __name__ == '__main__':
